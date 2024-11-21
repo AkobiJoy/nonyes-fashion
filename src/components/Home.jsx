e8bcd9495f73ed8ce2296d763bebb83303b98160
AkobiJoy <joyeugene3@gmail.com>
2024-11-21 13:20:25 +0100

    picture update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
        alt="picture" 
        width={700} 
        height={300} 
-       style={{ width: "500px", height: "640px", objectFit: "cover" }}
+      //  style={{ width: "500px", height: "640px", objectFit: "cover" }}
        className="shadow-lg shadow-sky-200  rounded-lg"
       />
      
