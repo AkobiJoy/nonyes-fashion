5051c57fd3c67d9430b95b630c880efd7c71785b
AkobiJoy <joyeugene3@gmail.com>
2024-11-26 11:06:34 +0100

    route correction

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       setLoading(true);
       // Handle form submission (e.g., API call)
       try {
-        const res = await axios.post("http://localhost:3000/api/login",{
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_ENDPONT}/login`,{
           email,
           password
         });
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -67,7 +67,7 @@ const Register = () => {
       setLoading(true);
       // Handle form submission (e.g., API call)
       try {
-        const res = await axios.post("http://localhost:3000/api/register",{
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_ENDPONT}/register`,{
           name,
           email,
           gender,
